5170c3e10590a2dd3ad5f39d7969f1964923c479
MartinAllsbrook <78395106+MartinAllsbrook@users.noreply.github.com>
2022-08-17 12:33:16 -0600

    Push

--- main.js
+++ main.js
@@ -16,12 +16,13 @@ window.addEventListener('DOMContentLoaded', (event) => {
         }
         // Object functions
         function select(index) {
-            console.log('select called'); // DEBUGGING
+            // console.log('select called'); // DEBUGGING
             let img = images[index].getElementsByTagName('img')[0]; // Get inner image object
 
             // Change div properties
             // images[index].style.transition = 'flex-shrink 1500ms ease-out';
             images[index].style.flexShrink = '0';
+            // images[index].style.width = 'none'
             images[index].style.filter = 'drop-shadow(0 0 6px #000000)';
             images[index].style.zIndex = '10';
             // images[index].style.flexGrow = '1';
@@ -32,12 +33,13 @@ window.addEventListener('DOMContentLoaded', (event) => {
             img.style.filter = 'saturate(1) brightness(1)';
         }
         function deselect(index) {
-            console.log('deselect called'); // DEBUGGING
+            // console.log('deselect called'); // DEBUGGING
             let img = images[index].getElementsByTagName('img')[0]; // Get inner image object
 
             // Change div properties
             // images[index].style.transition = 'flex-shrink 500ms ease-out';
             images[index].style.flexShrink = '1';
+            // images[index].style.width = '100%'
             images[index].style.filter = 'none';
             images[index].style.zIndex = '0';
             // images[index].style.flexGrow = '0';
@@ -47,6 +49,19 @@ window.addEventListener('DOMContentLoaded', (event) => {
             // Change image properties
             img.style.filter = 'saturate(0.25) brightness(0.6)';
         }
+    }
+
+    // Makes contact button clickable
+    {
+        document.getElementById('contact').addEventListener('click', () => {
+            document.getElementsByClassName('contact')[0].style.display = 'flex';
+            // document.getElementsByClassName('contact')[0].style.zIndex = '20';
+        }, false)
+
+        document.getElementById('close').addEventListener('click', () => {
+            document.getElementsByClassName('contact')[0].style.display = 'none';
+            // document.getElementsByClassName('contact')[0].style.zIndex = '20';
+        }, false)
     }
 }, false);
 
